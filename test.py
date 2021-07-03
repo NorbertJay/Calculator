aca5a66cf5bd3a483843e01cd8e03e008533c5df
mao6530 <86298642+mao6530@users.noreply.github.com>
2021-07-03 16:22:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -10,6 +10,47 @@ class Test(unittest.TestCase):
         c.btn2_pressed()
         c.btn2_pressed()
         self.assertEqual(c.btnequal_pressed(), "24")
+        
+    def test_minus(self):
+        c = Calculator() 
+        c.btnclear_pressed()
+        c.btn2_pressed()
+        c.btnminus_pressed()
+        c.btn2_pressed()
+        c.btn2_pressed()
+        self.assertEqual(c.btnequal_pressed(), "-20")
+
+    def test_multiply(self):
+        c = Calculator() 
+        c.btnclear_pressed()
+        c.btn2_pressed()
+        c.btnmultiply_pressed()
+        c.btn2_pressed()
+        self.assertEqual(c.btnequal_pressed(), "4")
+    
+    def test_divide(self):
+        c = Calculator() 
+        c.btnclear_pressed()
+        c.btn2_pressed()
+        c.btndivide_pressed()
+        c.btn2_pressed()
+        self.assertEqual(c.btnequal_pressed(), "1.0")
+    
+    def test_multiple_operations(self):
+        c = Calculator()
+        c.btnclear_pressed() 
+        c.btn9_pressed()
+        c.btn9_pressed()
+        c.btnminus_pressed()
+        c.btn3_pressed()
+        c.btn6_pressed()
+        c.btndivide_pressed()
+        c.btn9_pressed()
+        c.btnplus_pressed()
+        c.btn3_pressed()
+        c.btnmultiply_pressed()
+        c.btn6_pressed()
+        self.assertEqual(c.btnequal_pressed(), "113.0")
 
 
 if __name__ == "__main__":
