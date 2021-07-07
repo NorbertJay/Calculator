0e31180f7aaa5c4806af027b3d726e98033581c6
mao6530 <86298642+mao6530@users.noreply.github.com>
2021-07-07 17:24:14 +0800

    Update test.py

--- test.py
+++ test.py
@@ -51,6 +51,122 @@ class Test(unittest.TestCase):
         c.btnmultiply_pressed()
         c.btn6_pressed()
         self.assertEqual(c.btnequal_pressed(), "113.0")
+        
+    def test_dot_del_par(self):
+        c = Calculator()
+        c.btnclear_pressed() 
+        c.bl_pressed()
+        c.btn9_pressed()
+        c.btn9_pressed()
+        c.btnminus_pressed()
+        c.btn3_pressed()
+        c.btn6_pressed()
+        c.br_pressed()
+        c.btndivide_pressed()
+        c.btn9_pressed()
+        c.btnplus_pressed()
+        c.btn3_pressed()
+        c.btnmultiply_pressed()
+        c.btn6_pressed()
+        c.btnmultiply_pressed()
+        c.btn1_pressed()
+        c.btn0_pressed()
+        c.del_pressed()
+        self.assertEqual(c.btnequal_pressed(), "25.0")
+    
+    def test_power(self):
+        c = Calculator() 
+        c.btnclear_pressed()
+        c.btn9_pressed()
+        c.pow_pressed()
+        c.btn1_pressed()
+        self.assertEqual(c.btnequal_pressed(), "9")
+
+    def test_sin(self):
+        c = Calculator() 
+        c.btnclear_pressed()
+        c.btn0_pressed()
+        c.sin_pressed()
+        self.assertEqual(c.btnequal_pressed(), "0.0")
+    
+    def test_sin2(self):
+        c = Calculator() 
+        c.btnclear_pressed()
+        c.btn6_pressed()
+        c.btn9_pressed()
+        c.sin_pressed()
+        self.assertEqual(c.btnequal_pressed(), "-0.11478481378318722")
+    
+    def test_cos(self):
+        c = Calculator() 
+        c.btnclear_pressed()
+        c.btn0_pressed()
+        c.cos_pressed()
+        self.assertEqual(c.btnequal_pressed(), "1.0") 
+    
+    def test_cos2(self):
+        c = Calculator() 
+        c.btnclear_pressed()
+        c.btn4_pressed()
+        c.btn5_pressed()
+        c.cos_pressed()
+        self.assertEqual(c.btnequal_pressed(), "0.5253219888177297") 
+        
+    
+    def test_tan(self):
+        c = Calculator() 
+        c.btnclear_pressed()
+        c.btn0_pressed()
+        c.tan_pressed()
+        self.assertEqual(c.btnequal_pressed(), "0.0") 
+
+    def test_tan2(self):
+        c = Calculator() 
+        c.btnclear_pressed()
+        c.btn5_pressed()
+        c.tan_pressed()
+        self.assertEqual(c.btnequal_pressed(), "-3.380515006246586") 
+        
+    def test_round(self):
+        c = Calculator() 
+        c.btnclear_pressed()
+        c.btn6_pressed()
+        c.btn6_pressed()
+        c.dot_pressed()
+        c.btn1_pressed()
+        self.assertEqual(c.round_pressed(), "66") 
+    
+    def test_logarithm(self):
+        c = Calculator() 
+        c.btnclear_pressed()
+        c.btn1_pressed()
+        c.btn0_pressed()
+        c.btn0_pressed()
+        c.logarithm_pressed()
+        self.assertEqual(c.round_pressed(), "2")
+    
+    def test_par(self):
+        c = Calculator
+        c.btnclear_pressed(c)
+        c.btn1_pressed(c)
+        c.btnplus_pressed(c)
+        c.bl_pressed(c)
+        c.btn3_pressed(c)
+        c.btnminus_pressed(c)
+        c.btn2_pressed(c)
+        c.btnmultiply_pressed(c)
+        c.btn4_pressed(c)
+        c.br_pressed(c)
+        self.assertEqual(c.btnequal_pressed(c), "-4")
+    
+    def test_ln(self):
+        c = Calculator
+        c.btnclear_pressed(c)
+        c.btn5_pressed(c)
+        c.ln_pressed(c)
+        self.assertEqual(c.btnequal_pressed(c), "1.6094379124341003")
+    
+
 
 
 if __name__ == "__main__":
